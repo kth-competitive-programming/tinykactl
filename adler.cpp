afa8acf0f4faffbd3d5fc400f6fc4a9dcb1b5ab2
u1htlkd7 <u1htlkd7@karmosin08.nada.kth.se>
2002-09-16 18:39:18 +0200

    adler.cpp:   Changed to hex output
    bk: 3d8609363RcwZSRkcwmN-9wCDxo8nw

--- adler.cpp
+++ adler.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-int main() {  // "Adler-32 by Mark Adler" -> 1092552393
+int main() {  // "Adler-32 by Mark Adler" -> 411f06c9
   unsigned long crcbase = 65521, s1 = 1, s2 = 0; unsigned char c;
   while (cin >> c) s1 = (s1 + c) % crcbase, s2 = (s2 + s1) % crcbase;
-  cout << (s2 << 16 | s1) << endl; return 0;
+  cout << hex << (s2 << 16 | s1) << endl; return 0;
 }
