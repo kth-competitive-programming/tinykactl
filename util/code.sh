982d3002194b2c15396f216290ce68536ff2859b
mad33 <mad33@mad33>
2002-09-18 14:28:50 +0200

    Checksums!
    bk: 3d887182BwEP5H98ApNK_neN4yrxvQ

--- util/code.sh
+++ util/code.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 for i in numerical graph/mst; do
   for src in "$i"/*.cpp; do
-    dst=$(echo "$src" | cut -d. -f1).lg
+    base=$(echo "$src" | cut -d. -f1)
+    dst=$base.lg
+    sum=$base.sum
     case "$1" in
       gen)
 	get "$src"
 	util/stripcode header < "$src" |
 	util/lgrind/source/lgrind -i - > "$dst"
+	util/stripcode header preproc comments < "$src" |
+	util/adler > "$sum"
 	;;
       clean)
-	echo "rm -f $dst"
-	rm -f "$dst"
+	echo "rm -f $dst $sum"
+	rm -f "$dst" "$sum"
     esac
   done
 done
