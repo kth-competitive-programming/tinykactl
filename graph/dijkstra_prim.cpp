80e610dd1b417b2376fb52718ccfeef2e44816aa
f99-dry <f99-dry@grey04.nada.kth.se>
2002-09-26 19:06:35 +0200

    Documentation and dijkstra_prim update to make it compile.
    bk: 3d933e9bKIxbfbMCbbdenbAQnUB8HA

--- graph/dijkstra_prim.cpp
+++ graph/dijkstra_prim.cpp
@@ -8,29 +8,33 @@
  *   by Mattias de Zalenski
  */
 #include <set>
-#include "mpq"
 
 template <class V> struct idxless {
   const V &v;
   idxless(const V &_v) : v(_v) { }
-  bool operator<(int i, int j) const { return v[i] < v[j]; }
+  bool operator()(int i, int j) const { return v[i] < v[j]; }
 };
 /*
 template <class V, class C> struct idxcomp {
   const V &v; const C &c;
   idxcomp(const V &_v, const C &_c) : v(_v), c(_c) { }
-  bool operator<(int i, int j) const { return c(v[i], v[j]); }
+  bool operator()(int i, int j) const { return c(v[i], v[j]); }
 };
 */
 
-template <class E, class M, class P, class D, class T = M::value_type>
-void dijkstra_prim(int start, E &edges, M &min, P &path, D distfun,
-		   T inf = T(1 << 29), bool mst = false) {
+template <class E, class M, class P, class D>
+void dijkstra_prim(int start, E &edges, M &min, P &path, int n, D distfun,
+		   bool mst = false) {
+  typedef typename M::value_type T;
+  T inf(1<<29);
+
   typedef idxless< M > C;
   set< int, C > q(C(min)); // use a set as a modifiable priority queue
 
-  for (int i = 0; i < edges.size(); i++)
+  for (int i = 0; i < n; i++) {
     min[i] = inf;
+    path[i] = -1;
+  }
   min[start] = T();
 
   q.insert(start);
@@ -38,9 +42,11 @@ void dijkstra_prim(int start, E &edges, M &min, P &path, D distfun,
     int node = *q.begin();
     q.erase(q.begin());
     if (mst) min[node] = T(); // only difference between dijkstra and prim
-    typedef E::value_type L;
-    L &l = edges[node];
-    for (L::iterator it = l.begin(); it != l.end(); it++) {
+    typedef typename E::value_type L;
+    typedef typename L::const_iterator L_iter;
+
+    const L &l = edges[node];
+    for (L_iter it = l.begin(); it != l.end(); ++it) {
       pair<int, T> p = distfun(*it, min[node]);
       int dest = p.first; T dist = min[node] + p.second;
       if (dist < min[dest]) {
@@ -53,41 +59,13 @@ void dijkstra_prim(int start, E &edges, M &min, P &path, D distfun,
   }
 }
 
-template <class E, class M, class P, class D, class T = M::value_type>
-void dijkstra_prim(int start, E &edges, M &min, P &path, D distfun,
-		   T inf = T(1 << 29), bool mst = false) {
-  typedef idxless< M > C;
-  typedef E::value_type L;
-
-  mpq< int, C > q(C(min)); // use a modifiable priority queue
-  for (int i = 0; i < edges.size(); i++)
-    min[i] = inf;
-  min[start] = T();
-
-  q.push(start);
-  while (!q.empty()) {
-    int node = q.front(); q.pop();
-    if (mst) min[node] = T(); // only difference between dijkstra and prim
-    L &l = edges[node];
-    for (L::iterator it = l.begin(); it != l.end(); it++) {
-      pair<int, T> p = distfun(*it, min[node]);
-      int dest = p.first; T dist = min[node] + p.second;
-      if (dist < min[dest]) {
-	q.update(dest, min[dest], dist);
-	path[dest] = node;
-      }
-    }
-  }
-}
-
-
 
 
 // one distfun
 pair<int, int> one_dist(int node, int) { return make_pair(node, 1); }
 
 // weighted edge distfun
-template <class P, class T> P w_dist(P edge, T) { return edge; }
+template <class P, class T> P id_dist(P edge, T) { return edge; }
 
 // for time-table searches without mod:
 template <class T, T inf>
@@ -111,4 +89,3 @@ struct mod_dist {
     return make_pair(edge.first, t - tmod + *it);
   }
 };
-
