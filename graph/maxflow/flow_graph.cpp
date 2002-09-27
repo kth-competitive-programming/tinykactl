d201e9c63c0546fdddf8765c35ec3d8f4ec3d7db
dary <dary@c213-100-36-18.swipnet.se>
2002-09-27 11:56:45 +0200

    Completed all max-flow algorithms. lift_to_front is now functioning and tested. Descriptions for all flow algorithms. Some other minor things...
    bk: 3d942b5dNk3NlwB_jR1uJIASJVzeyw

--- graph/maxflow/flow_graph.cpp
+++ graph/maxflow/flow_graph.cpp
@@ -1,53 +1,34 @@
-/*****************************************************************************
- * 
- * MaxFlow1: General structures for maxflow-algortihs
- * ==================================================
+/* KTH ACM Contest Template Library
  *
- * Find the maximum flow. The functions are all based on the FlowGraph
- * data structure which is a vector of vectors with FlowEdges. The
- * FlowEdge describes an edge with an allowed flow together with a
- * pointer (index) to its opposite back-edge.
+ * Graph/Network Flow/Flow Graph
+ *
+ * The functions are all based on a flow_graph which is a container
+ * with vectors with flow_edge's. The flow_edge describes an edge with an
+ * allowed flow together with a pointer (index) to its opposite back-edge.
  *
  * Flows are added to the graph with the add_edge function. NB! The
  * flow for are a edge and its back-edge must be given at the same time
  * and only once.
  *
- * The maximum flow are calculated by repetitive calls to increase_flow
- * or increase_flow1. The last function is an optimized version where
- * all augmenting paths have the flow 0/1 (e.g. a bipartite matching).
- *
- * increase_flow and increase_flow1 both have complexity O(E).
- *
- * ------------------------------------------------------------------------- *
- *
- * NADA acmlib (10 March 2002)
- * Templates for KTH-NADA, Ballons'R'Us, ACM 2001-2002
- *   Swedish competition, Link?ping 6 Oct 2001
- *   Revised for SWERC, Portu, Portugal 17 Nov 2001
- *   Revised for World Finals, Honolulu, 23 Mar 2002
- *   David Rydh, Mattias de Zalenski, Fredrik Niemel?
- *
- *****************************************************************************/
+ * Credit:
+ *   By David Rydh and Mattias de Zalenski
+ */
 
 #include <vector>
 
-/***************************************
- * General Max-flow structures/functions
- ***************************************/
 
-struct FlowEdge {
-  int  destNode;
-  int  cap;             // The amount of flow that this edge can allow
-  int  backEdgeIndex;   // The index of the back-edge in graph[destNode]
-
-  FlowEdge( int a=-1, int b=0, int c=-1 ) : destNode(a), cap(b),
-                                            backEdgeIndex(c) {}
+template <class T>
+struct flow_edge {
+  typedef T flow_type;
+  int dest, back; // back is the index of the back-edge in graph[dest]
+  T c, f; // capacity and flow
+  flow_edge(int _dest, int _back, T _c, T _f = T())
+    : dest(_dest), back(_back), c(_c), f(_f) { }
 };
 
-typedef vector< vector<FlowEdge> > FlowGraph;
-
-void flow_addedge( FlowGraph &g, int start, int end, int cap, int back_cap=0 ){
-  g[start].push_back( FlowEdge(end, cap, g[end].size()) );
-  g[end].push_back( FlowEdge(start, back_cap, g[start].size()-1) );
+template <class E, class T>
+void flow_add_edge(E &flow, int node, int dest, T c, T back_c = T()) {
+  flow[node].push_back(flow_edge<T>(dest, edges[dest].size(), c));
+  flow[dest].push_back(flow_edge<T>(node, edges[node].size() - 1, back_c));
 }
 
