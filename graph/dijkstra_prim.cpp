57e826043afd0d96dd64b82808bf8a5fe94c7232
dary <dary@c213-100-36-18.swipnet.se>
2002-09-29 14:11:16 +0200

    Fixed time-table search in dijkstra_prim. Added Valladolid-problem 10362 (uses time-table search).
    bk: 3d96ede4NH0hqvZNXkS0I3x1kn27xg

--- graph/dijkstra_prim.cpp
+++ graph/dijkstra_prim.cpp
@@ -6,6 +6,7 @@
  *   Dijkstra
  *   Prim
  *   by Mattias de Zalenski
+ *   Modified by David Rydh
  */
 #include <set>
 
@@ -70,23 +71,14 @@ template <class P, class T> P id_dist(P edge, T) { return edge; }
 
 // for time-table searches without mod:
 template <class T, T inf>
-pair<int, T> step_dist(pair<int, T> &edge, T t) {
-  return make_pair(edge.first, edge.second < t ? inf : edge.second - t);
+pair<int, T> step_dist(pair<int, pair<T, T> > &edge, T t) {
+  return make_pair(edge.first, edge.second.first < t ? inf :
+		   edge.second.first - t + edge.second.second);
 }
 
 // for time-table searches:
 template <int MOD>
-pair<int, int> mod_dist(pair<int, int> &edge, int t) {
-  return make_pair(edge.first, (edge.second - t % MOD + MOD) % MOD);
+pair<int, int > mod_dist(const pair<int, pair<int, int> > &edge, int t) {
+  return make_pair(edge.first, (edge.second.first - t % MOD + MOD) % MOD +
+    edge.second.second);
 }
-
-// for time-table searches with lots of times per connection:
-struct mod_dist {
-  int mod; mod_dist(int _mod) : mod(_mod) { }
-  pair<int, int> operator()(pair<int, set< int > > &edge, int t) {
-    const set<int> &tbl = edge.second; int tmod = t % mod;
-    set<int>::const_iterator it = tbl.lower_bound(tmod);
-    if (it == tbl.end()) t += mod, it = tbl.begin();
-    return make_pair(edge.first, t - tmod + *it);
-  }
-};
