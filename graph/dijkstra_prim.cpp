0328bed3b4b4079a1ac34f63f262cb11b1f36294
mad33 <mad33@mad33.>
2002-10-09 00:22:06 +0200

    Fixed dijkstra_prim bug.
    bk: 3da35a8eB0bYzPDSIellD-Qrt0fz0A

--- graph/dijkstra_prim.cpp
+++ graph/dijkstra_prim.cpp
@@ -10,12 +10,12 @@
  */
 #include <set>
 
+/*
 template <class V> struct idxless {
   const V &v;
   idxless(const V &_v) : v(_v) { }
   bool operator()( const int &i, const int &j ) const { return v[i] < v[j]; }
 };
-/*
 template <class V, class C> struct idxcomp {
   const V &v; const C &c;
   idxcomp(const V &_v, const C &_c) : v(_v), c(_c) { }
@@ -29,9 +29,7 @@ void dijkstra_prim(E &edges, M &min, P &path, int start, int n, D distfun,
   typedef typename M::value_type T;
   T inf(1<<29);
 
-  typedef idxless< M > C;
-  C comp( min );
-  set< int, C > q(comp); // use a set as a modifiable priority queue
+  set< pair<T, int> > q; // use a set as a modifiable priority queue
 
   for (int i = 0; i < n; i++) {
     min[i] = inf;
@@ -39,9 +37,9 @@ void dijkstra_prim(E &edges, M &min, P &path, int start, int n, D distfun,
   }
   min[start] = T();
 
-  q.insert(start);
+  q.insert(make_pair(min[start], start));
   while (!q.empty()) {
-    int node = *q.begin();
+    int node = q.begin()->second;
     q.erase(q.begin());
     if (mst) min[node] = T(); // only difference between dijkstra and prim
     typedef typename E::value_type L;
@@ -52,9 +50,9 @@ void dijkstra_prim(E &edges, M &min, P &path, int start, int n, D distfun,
       pair<int, T> p = distfun(*it, min[node]);
       int dest = p.first; T dist = min[node] + p.second;
       if (dist < min[dest]) {
-	q.erase(dest);    //
+	q.erase(make_pair(min[dest], dest)); //
 	min[dest] = dist; // update dest in the queue
-	q.insert(dest);   //
+	q.insert(make_pair(min[dest], dest)); //
 	path[dest] = node;
       }
     }
