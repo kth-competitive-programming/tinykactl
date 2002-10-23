b09a8a2dac428d92884433a77780899b234da03c
mad33 <mad33@mad33.>
2002-10-23 21:53:31 +0200

    Fixed mpq, added sign (sign extends a number).
    bk: 3db6fe3b8vTnzRqZza_IJopQc-AD4g

--- datastructures/mpq.cpp
+++ datastructures/mpq.cpp
@@ -6,17 +6,18 @@
  *   By Mattias de Zalenski
  */
 
+#include <algorithm>
 #include <set>
 
-template <class T, class C>
+template <class T, class C = greater<T> > // priority_queue compatibility..
 struct mpq {
   set<T, C> s;
   mpq(const C &c = C()) : s(c) { }
-  void empty() { return s.empty(); }
+  bool empty() { return s.empty(); }
   void push(const T &x) { s.insert(x); }
   const T &front() { return *s.begin(); }
-  template <class V>
-  void update(const T &x, V &oldv, const V &newv) {
+  template <class S>
+  void update(const T &x, S &oldv, const S &newv) {
     s.erase(x);
     oldv = newv;
     s.insert(x);
