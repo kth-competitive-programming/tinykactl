ac043beb3d98f877005d1ced15024ae781c95e46
d00-fni <d00-fni@karmosin03.nada.kth.se>
2002-10-24 10:50:04 +0200

    Small changes. Moved mulmod and expmod, fixed spelling of Stirling...
    bk: 3db7b43cn3SucJLdbPjm8xUkugfSsw

--- numerical/stirling.cpp
+++ numerical/stirling.cpp
@@ -1,20 +1,20 @@
 /* KTH ACM Contest Template Library
  *
- * Numerical/Combinatorics/Sterling numbers
+ * Numerical/Combinatorics/Stirling numbers
  *
- * beräknar sterlingtalet för n,k
+ * beräknar stirlingtalet för n,k
  * dvs hur många sätt kan man placera n olika objekt i k lådor
  * utan att lämna någon låda tom
  *
  * Credit:
- *   Sterling
+ *   Stirling
  *   By Max Bennedich
  *   Update by David Rydh
  */
 
-int sterling( int n, int k ) {
+int stirling( int n, int k ) {
   if( n <= 1 )
     return k==n ? 1:0;
   else
-    return sterling( n-1, k-1 ) + k*sterling( n-1, k );
+    return stirling( n-1, k-1 ) + k*stirling( n-1, k );
 }
