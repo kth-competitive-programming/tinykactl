bad27982fb957c1af63bd18b14189fc9064b7ce9
mad33 <mad33@mad33.>
2002-10-30 00:25:48 +0100

    Moved operators and short io inside rational - it was far too long. Tried out a few other things instead; see code and delta comment.
    bk: 3dbf18fcoa4vDzj0BznASsJrr9Ez3A

--- numerical/rational.cpp
+++ numerical/rational.cpp
@@ -3,78 +3,53 @@
  * Numerical/rational
  *
  * Credit:
- *   By Fredrik Niemelä
+ *   By Fredrik Niemelä, Mattias de Zalenski
  */
 
 #include "gcd.cpp"
 
 template <class T>
 struct rational {
+  typedef rational<T> rT;
+  typedef const rT & R;
   T n, d;
   rational(T _n=T(), T _d=T(1)) : n(_n), d(_d) { normalize(); }
   void normalize() {
     T f = gcd(n, d); n /= f; d /= f;
     if (d < T()) n *= -1, d *= -1;
   }
-  bool operator <(const rational<T> &r) const { return n*r.d < d*r.n; }
-  bool operator ==(const rational<T> &r) const { return n*r.d == d*r.n; }
+  bool operator < (R r) const { return n * r.d <  d * r.n; }
+  bool operator ==(R r) const { return n * r.d == d * r.n; }
+
+  rT operator -() { return rT(-n, d); }
+
+  rT operator +(R r) { return rT(n*r.d + r.n*d, d*r.d); }
+  rT operator -(R r) { return rT(n*r.d - r.n*d, d*r.d); }
+
+  rT operator *(R r) { return rT(n*r.n, d*r.d); }
+  rT operator /(R r) { return rT( n*r.d,/**/d*r.n); }
+  T/**//**/ div(R r) { return/**/(n*r.d) / (d*r.n); }
+  rT operator %(R r) { return rT((n*r.d) % (d*r.n), d*r.d); }
+
+  rT operator <<(int b) { return b<0 ? a>>-b : rT(n<<b, d); }
+  rT operator >>(int b) { return b<0 ? a<<-b : rT(n, d<<b); }
+
+  ostream &print_frac(ostream &out) {
+    out << n; if (d != T(1)) out << '/' << d;
+    return out;
+  }
+
+  istream &read_frac(istream &in) {
+    in >> n;
+    if (in.peek() == '/') { char c; in >> c >> d; } else d = T(1);
+    normalize();
+    return in;
+  }
 };
 
 template <class T>
-rational<T> operator +(const rational<T> &a, const rational<T> &b) {
-  return rational<T>(a.n*b.d + b.n*a.d, a.d*b.d);
-}
-
-template <class T>
-rational<T> operator -(const rational<T> &a) {
-  return rational<T>(-a.n, a.d);
-}
-
-template <class T>
-rational<T> operator -(const rational<T> &a, const rational<T> &b) {
-  return rational<T>(a.n*b.d - b.n*a.d, a.d*b.d);
-}
-
-template <class T>
-rational<T> operator *(const rational<T> &a, const rational<T> &b) {
-  return rational<T>(a.n*b.n, a.d*b.d);
-}
-
-template <class T>
-rational<T> operator /(const rational<T> &a, const rational<T> &b) {
-  return rational<T>(a.n*b.d, a.d*b.n);
-}
-
-template <class T>
-T div(const rational<T> &a, const rational<T> &b) {
-  return (a.n*b.d) / (a.d*b.n);
-}
-
-template <class T>
-rational<T> operator %(const rational<T> &a, const rational<T> &b) {
-  return rational<T>((a.n*b.d) % (a.d*b.n), a.d*b.d);
-}
-
-template <class T>
-rational<T> operator <<(const rational<T> &a, int b) {
-  return b < 0 ? a >> -b : rational<T>(a.n << b, a.d);
-}
-
-template <class T>
-rational<T> operator >>(const rational<T> &a, int b) {
-  return b < 0 ? a << -b : rational<T>(a.n, a.d << b);
-}
-
-template <class T>
-ostream &print_frac(ostream &out, const rational<T> &r) {
-  out << r.n;
-  if (r.d != 1) out << '/' << r.d;
-  return out;
-}
-
-template <class T>
 ostream &print_dec(ostream &out, const rational<T> &r,
-		   int radix = 10, int precision = 15) {
+		   int precision = 15, int radix = 10) {
   T n = r.n, d = r.d;
   if (n < T()) out << '-', n *= -1;
   out << n/d; n %= d;
@@ -88,20 +63,9 @@ ostream &print_dec(ostream &out, const rational<T> &r,
   return out;
 }
 
-template <class T>
-ostream &operator <<(ostream &out, const rational<T> &r) {
+template <class T> ostream &operator <<(ostream &out, const rational<T> &r) {
+  //return r.print_frac(out);
   return print_dec(out, r);
-}
-
-template <class T>
-istream &read_frac(istream &in, rational<T> &r) {
-  in >> r.n;
-  if (in.peek() == '/') {
-    char c; in >> c >> r.d;
-  } else
-    r.d = T(1);
-  r.normalize();
-  return in;
 }
 
 template <class T>
@@ -121,7 +85,7 @@ istream &read_dec(istream &in, rational<T> &r) {
   return in;
 }
 
-template <class T>
-istream &operator >>(istream &in, rational<T> &r) {
+template <class T> istream &operator >>(istream &in, rational<T> &r) {
+  //return r.read_frac(in);
   return read_dec(in, r);
 }
