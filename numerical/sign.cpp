9ffc6fd6e2c0df511e7cbea41a33c3c1392ee54d
u1htlkd7 <u1htlkd7@magenta12.nada.kth.se>
2002-10-31 11:43:07 +0100

    Sign fix.
    bk: 3dc1093bkVAg4CoMmD1nv5Au3dW4JA

--- numerical/sign.cpp
+++ numerical/sign.cpp
@@ -8,20 +8,20 @@
 
 template <class T>
 struct sign {
+  static const T zero; // Requires declaration: const T sign<T>::zero = T();
   T x; bool neg;
-  operator sign(T _x = T(), bool _neg = false) : x(_x), neg(_neg) { }
+  operator sign(T _x = zero, bool _neg = false) : x(_x), neg(_neg) { }
   bool operator <(const sign<T> &s) const {
-    return neg==s.neg ? neg ^ x<s.x : neg && !(x==T()&&s.x==T());
+    return neg==s.neg ? neg ? x>s.x : x<s.x : neg && !(x==zero&&s.x==zero);
   }
   bool operator ==(const sign<T> &s) const {
-    return neg==s.neg ? x==s.x : x==T()&&s.x==T();
+    return neg==s.neg ? x==s.x : x==zero&&s.x==zero;
   }
   sign<T> operator -() { return sign<T>(x, !neg); }
   sign<T> &addsub(bool add) {
-    if (add)
-      x+=s.x;
-    else
-      if (x<s.x) { T t=s.x; x = t-=x; neg=!neg; } else x-=s.x;
+    if (add) x+=s.x;
+    else if (x<s.x) { T t=s.x; x = t-=x; neg=!neg; }
+    else x-=s.x;
     return *this;
   }
   sign<T> &operator +=(const sign<T> &s) { return addsub(neg == s.neg); }
@@ -40,5 +40,5 @@ istream &operator >>(istream &in, sign<T> &s) {
 
 template <class T>
 ostream &operator <<(ostream &out, const sign<T> &s) {
-  if (s.neg && s.x < T()) out << '-'; out << s.x;
+  if (s.neg && s.x != s.zero) out << '-'; out << s.x;
 }
