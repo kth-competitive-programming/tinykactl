449b7b52ec3171269e4148b8e6ae957c51a5d1d3
mad33 <mad33@mad33>
2002-11-04 01:17:43 +0100

    Changed exptmod to expmod, expt to exp, added exp listing.
    bk: 3dc5bca7uB9FC9klTr9dUfE4GtON8w

--- numerical/exp.cpp
+++ numerical/exp.cpp
@@ -6,13 +6,13 @@
  *   By Mattias de Zalenski
  */
 
-template <class B, class T>
-B exp(B b, T e) {
-  B r(1);
-  if (e & 1) r = s;
+template <class B, class E>
+B exp(B b, E e) {
+  B r = 1;
+  if (e & 1) r = b;
   while (e > 1) {
-    e >>= 1, s *= s;
-    if (e & 1) r *= s;
+    e >>= 1, b *= b;
+    if (e & 1) r *= b;
   }
   return r;
 }
