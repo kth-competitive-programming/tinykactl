61d3b79ddfda2a133a649fb54e1ddaee9a31d74f
dary <dary@dary.zapto.org>
2002-11-06 19:03:35 +0100

    Fixed bugs in stack. Added vectorstack.
    bk: 3dc95977HfFifxZlK6sPsugQB1xZOQ

--- tinystl/stack.cpp
+++ tinystl/stack.cpp
@@ -1,8 +1,18 @@
+/* KTH ACM Contest Template Library
+ *
+ * tinystl/stack
+ *
+ * Credit:
+ *   Alexander Stepanov
+ *   Based on STL
+ *   By Mattias de Zalenski
+ */
+
 template <class T>
 struct stack_entry {
   typedef stack_entry<T> sT;
   T x; sT *next;
-  stack_entry(T _x, sT *_next = 0) : x(_x), next(_next) { }
+  stack_entry(const T &_x, sT *_next = 0) : x(_x), next(_next) { }
 };
 
 template <class T>
@@ -10,9 +20,10 @@ struct stack {
   typedef T value_type;
   typedef stack_entry<T> sT;
   sT *root; stack() : root(0) { }
+  ~stack() { while( !empty() ) pop(); }
   bool empty() { return root == 0; }
-  void push(T x) { root = new sT(x, root); }
-  void pop() { root = root.next; }
+  void push(const T &x) { root = new sT(x, root); }
+  void pop() { sT *o=root; root = root.next; delete o; }
   T &top() { return root.x; }
   const T &top() const { return root.x; }
 };
