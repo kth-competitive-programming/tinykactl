ad0a4542f9a3f3a0c7766f898a821d80b1b046b8
dary <dary@dary.zapto.org>
2002-11-06 18:48:05 +0100

    Added vector datatype. Added copy, copy_backward and fill.
    bk: 3dc955d5GefXvA8AdRfOtoVyAhkikQ

---
+++ tinystl/copy.cpp
@@ -0,0 +1,31 @@
+/* KTH ACM Contest Template Library
+ *
+ * tinystl/copy
+ *
+ * Credit:
+ *   Alexander Stepanov
+ *   Based on STL
+ *   By David Rydh
+ */
+
+template<class R, class S>
+S copy(R f, R l, S d) {
+  while( f<l ) *d++ = *f++;
+  return d;
+}
+
+template<class R, class S>
+S copy_backward(R f, R l, S d) {
+  while( f<l ) *--d = *--l;
+  return d;
+}
+
+template<class R, class T>
+void fill(R f, R l, const T &val) {
+  while( f<l ) *f++ = val;
+}
+
+template<class R, class T>
+void fill_n(R f, int n, const T &val) {
+  while( n-- > 0 ) *f++ = val;
+}
