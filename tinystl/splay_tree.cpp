131313faecaa859da99364ac3488850fcb0b214d
u1htlkd7 <u1htlkd7@karmosin10.nada.kth.se>
2002-11-11 15:59:27 +0100

    Moved split and join into splay tree insert and erase. Shortened pair..
    bk: 3dcfc5cfHHb-m2nyusEXIYi1AXPx4Q

--- tinystl/splay_tree.cpp
+++ tinystl/splay_tree.cpp
@@ -18,9 +18,12 @@ struct splay_node {
 template <class T>
 struct splay_tree {
   typedef splay_node<T> *P;
+
   typedef T value_type;
   typedef P iterator;
+
   P root; splay_tree() : root(0) { }
+
   static void rot(P i, bool left) {
     P j = i->c(!left), p = i->p;
     if (p) p->c(i == p->l) = j;
@@ -39,16 +42,12 @@ struct splay_tree {
       }
     return i;
   }
-  static void split(P i, P &l, P &r) {
-    l = splay(i);
-    r = l->r; l->r = 0;
-    if (r) r->p = 0;
-  }
   static void join(P l, P r, P &i) {
     if (l) while (l->r) rot(l, true), l = l->p;
     if (l) l->r = r; if (r) r->p = l;
     i = l ? l : r;
   }
+
   bool empty() { return root == 0; }
   iterator find(const T &x) {
     P p = root, i = root;
@@ -64,9 +63,10 @@ struct splay_tree {
   }
   void insert(const T &x) {
     if (root) {
-      //while (i) p = i, i = p->c(x < p->x);
       find(x);
-      P l, r; split(root, l, r);
+      P l, r; // split:
+      if (x < root->x) r = root, l = r->l, r->l = 0;
+      else /**//**/ l = root, r = l->r, l->r = 0;
       root = new splay_node<T>(x, l, r);
       if (l) l->p = root;
       if (r) r->p = root;
@@ -74,10 +74,18 @@ struct splay_tree {
     else
       root = new splay_node<T>(x);
   }
-  void erase(iterator i) {
+  /*void erase(iterator i) {
     splay(i);
     join(i->l, i->r, root);
     if (root) root->p = 0;
+    delete i;
+    }*/
+  void erase(iterator i) {
+    splay(i);
+    P l = i->l, r = i->r; // join:
+    if (l) while (l->r) rot(l, true), l = l->p;
+    if (l) l->r = r, l->p = 0; if (r) r->p = l;
+    root = l ? l : r;
     delete i;
   }
 };
--- tinystl/utility.cpp
+++ tinystl/utility.cpp
@@ -16,16 +16,13 @@ template <class S, class T>
 struct pair {
   typedef S first_type;
   typedef T second_type;
-  S first; T second;
-
-  pair() : first(S()), second(T()) { }
+  typedef const pair<S, T> &R;
+  S first; T second; pair() : first(S()), second(T()) { }
   pair(const S &s, const T &t) : first(s), second(t) { }
-  template<class U, class V>
-  pair(const pair<U,V> &p) : first(p.first), second(p.second) { }
-
-  bool operator==(const S &x ) { return first==x.first && second==x.second; }
-  bool operator<( const S &x ) { return first<x.first ||
-				   (!(x.first<first) && second < x.second); }
+  //template <class P> pair(const P &p) : first(p.first), second(p.second) { }
+  bool operator==(R p) { return first==p.first && second==p.second; }
+  bool operator<(R p) { return first<p.first ||
+			  first<=p.first && second<p.second; }
 };
 
 template <class S, class T>
