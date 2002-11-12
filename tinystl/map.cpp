a35520776b3fe981bee536b9129deeab171dc815
u1htlkd7 <u1htlkd7@grey07.nada.kth.se>
2002-11-12 19:36:17 +0100

    Converted (multi)map to associative, which handles both (multi)map and (multi)set.
    bk: 3dd14a21nTVy1_UH_e1cvwWTXsF4RA

--- tinystl/map.cpp
+++ tinystl/map.cpp
@@ -10,37 +10,28 @@
 #include "utility.cpp"
 #include "splay_tree.cpp"
 
-template <class K, class V, class C = less<K>, bool multi=false >
-struct map {
+template <class K, class V, class C, bool multi >
+struct associative {
   typedef K key_type;
-  typedef pair<const K, V> value_type;
+  typedef V value_type;
   typedef const value_type &vR;
-  typedef C key_compare;
 
-  struct value_compare { // : public binary_function<V, V, bool>
-    C comp; value_compare(C _comp) : comp(_comp) { }
-    bool operator()(vR x, vR y) { return comp(x.first, y.first); }
-  } comp;
-
-  typedef splay_tree<value_type, value_compare> Tree; 
+  typedef splay_tree<value_type, value_compare> Tree;
   Tree t;
   typedef splay_node_iterator<value_type, false> iterator;
   typedef splay_node_iterator<value_type, true> reverse_iterator;
 
-  map(C key_comp = C()) : comp(value_compare(key_comp)), t(comp) { }
+  associative(C _comp = C()) : comp(_comp), t(_comp) { }
+
+  virtual V k2v(const K &k) = 0;
 
   // accessors
-  key_compare key_comp() const { return comp.comp; }
-  value_compare value_comp() const { return comp; }
   iterator begin() { return iterator::begin(t.root); }
   iterator end() { return iterator::end(t.root); }
   reverse_iterator rbegin() { return reverse_iterator::begin(t.root); }
   reverse_iterator rend() { return reverse_iterator::end(t.root); }
   bool empty() const { return t.empty(); }
   unsigned size() const { return t.n; }
-  V &operator[](const K &k) {
-    return (*insert(make_pair(k, V())).first).second;
-  }
 
   // insert/erase
   pair<iterator, bool> insert(vR x) {
@@ -60,7 +51,7 @@ struct map {
 
   // map operations
   iterator find(const K &k, bool left = true) { // return first match?? (/stl)
-    return iterator(t.find(value_type(k, V()), left), t.root);
+    return iterator(t.find(k2v(k), left), t.root);
   }
   unsigned count(const K &k) {
     if (multi) {
@@ -75,12 +66,12 @@ struct map {
   }
   iterator lower_bound(const K &k) {
     find(k, true); iterator i(t.root);
-    if (key_comp()(i->first, k)) ++i;
+    if (comp(i, k2v(k))) ++i;
     return i;
   }
   iterator upper_bound(const K &k) {
     find(k, false); iterator i(t.root);
-    if (!key_comp()(k, i->first)) ++i;
+    if (!comp(k2v(k), i)) ++i;
     return i;
   }
   pair<iterator, iterator> equal_range(const K &k) {
@@ -88,7 +79,35 @@ struct map {
   }
 };
 
-template <class K, class V, class C = less<K> >
-struct multimap : public map<K, V, C, true> {
-  multimap(C key_comp=C()) : map<K, V, C, true>(key_comp) { }
+template <class vR, class C>
+struct map_compare { // : binary_function<V, V, bool>
+  C comp; map_compare(C _comp) : comp(_comp) { }
+  bool operator()(vR x, vR y) { return comp(x.first, y.first); }
+} comp;
+
+
+template <class K, class V, class C = less<K>, bool multi=false,
+  class KV = pair<const K, V>,
+  class AC = map_compare<const KV &, C>,
+  class A = associative<K, KV, AC, multi> >
+struct map : A {
+  map(C key_comp = C()) : A(AC(key_comp)) { }
+  V &operator[](const K &k) {
+    return (*insert(k2v(k)).first).second;
+  }
+  virtual KV k2v(const K &k) { return make_pair(k,V()); }
 };
+
+template <class T, class C = less<T>, bool multi=false,
+  class A = associative<T, T, C, multi>
+struct set : A {
+  virtual T k2v(const T &k) { return k; }
+};
+
+
+
+template <class K, class V, class C = less<K>, class M = map<K, V, C, true> >
+struct multimap : M { multimap(C key_comp = C()) : M(key_comp) { } };
+
+template <class T, class C = less<T>, class S = set<T, C, true> >
+struct multiset : S { multiset(C comp = C()) : S(comp) { } };
