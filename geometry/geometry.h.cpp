58168fc2a3b9ad8de742817f171ae489d68de611
u1htlkd7 <u1htlkd7@grey02.nada.kth.se>
2002-11-13 17:36:15 +0100

    Removed simple point, retemplatized line on P.
    bk: 3dd27f7fNnxRbc-PT0kdke3jeDmNeg

--- geometry/geometry.h.cpp
+++ geometry/geometry.h.cpp
@@ -1,6 +1,6 @@
 /* KTH ACM Contest Template Library
  *
- * Geometry/Primitives/pi, sqr, point simple, line unnecessary
+ * Geometry/Primitives/pi, sqr, line unnecessary
  *
  * Credit:
  *   by Mattias de Zalenski
@@ -10,21 +10,11 @@
 
 const double pi = acos(0) * 2;
 
+
 template <class T> T sqr(T x) { return x * x; }
 
 
-template <class T> struct point {
-  typedef T coord_type;
-  T x, y;
-  point(T _x=T(), T _y=T()) : x(_x), y(_y) { }
-  bool operator< (const point<T> &p) const {
-    return x < p.x || x <= p.x && y < p.y;
-  }
-};
-
-
-template <class T> struct line {
-  typedef T coord_type;
-  point<T> p1, p2;
-  line(point<T> _p1=point<T>(), point<T> _p2=point<T>() ):p1(_p1), p2(_p2) {}
+template <class P> struct line {
+  typedef typename P::coord_type coord_type;
+  P p1, p2; line(P _p1 = P(), P _p2 = P()) : p1(_p1), p2(_p2) { }
 };
