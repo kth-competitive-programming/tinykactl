8eaff97782503ec8351745a45441f7d02cf95383
d00-fni <d00-fni@grey05.nada.kth.se>
2002-11-13 12:15:06 +0100

    Added stream manipulators moved TinySTL to appendix
    bk: 3dd2343a4pxa1lbny87ymao90h-0Vg

---
+++ test/manipulators.cpp
@@ -0,0 +1,18 @@
+#include "Testplate.cpp"
+
+#include "../io/manipulators.cpp"
+
+void init() {}
+
+bool solve(int P) {
+  long n;
+
+  cin >> n;
+
+  expand<long> e(n);
+  cout << e << endl;
+  //  cout << expand<long>(n) << endl;
+
+  return true;
+} 
+
