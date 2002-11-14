801db3b52101643c71b052ae2da212b74355cd6a
d00-pgo <d00-pgo@grey06.nada.kth.se>
2002-11-14 14:31:30 +0100

    Fixed my syntax errors in tinystl.tex Revised regexp and automata part.
    bk: 3dd3a5b2z-f2Rsw4pH3W5Gmi48wm-g

--- pattern/nfa.cpp
+++ pattern/nfa.cpp
@@ -1,6 +1,6 @@
 /* KTH ACM Contest Template Library
  *
- * Automata Theory/Finite State Automata/NFA:s
+ * Pattern Matching/Automata/NFA
  *
  * Credit:
  *   By Per Austrin
@@ -9,85 +9,50 @@
 #include <queue> /* Used in the epsilon closure */
 #include <vector>
 #include <set>
-#include <string>
-
-/* Note: some general cleaning up between this file and regexp.cc is to
- *       be expected in the near future. I hope.
- */
 
 typedef vector<int> vi;
 typedef set<int> StateSet;
 
-class NFA {
-protected:
+struct NFA {
   typedef vector<vi> NFAState;
-  typedef vector<NFAState> NFAStates;
-  NFAStates states;
+  vector<NFAState> states;
+  vector<bool> accepting;
   
-  int parseRegExp(string::const_iterator& i, int entry) {
-    int res = entry;
-    char literal = 0;
-    while (*i && *i != ')') {
-      switch (*i) {
-      case '"':
-	literal = ~literal;
+  NFA() { }
+
+  size_t match(char* s) {
+    size_t *lastAccept = string::npos;
+    char *i = s;
+    StateSet states;
+    states.insert(0);
+    while (!states.empty()) {
+      epsClosure(S);
+      for (StateSet::iterator si = states.begin(); si != states.end(); ++si)
+	if (accepting[*si])
+	  lastAccept = i - s;
+      if (!*i)
 	break;
-      case '(':
-	res = parseRegExp(++i, res);
-	break;
-      case '|': {
-	int end = parseRegExp(++i, entry);
-	states[end][0].push_back(states.size());
-	states[res][0].push_back(states.size());
-	res = states.size();
-	states.push_back(NFAState(256, vi()));
-	--i;
-	break;
-      }
-      case '*':
-	states[res][0].push_back(entry);
-	res = entry;
-	break;
-      case '[':
-	while (*++i != ']' && *i) {
-	  states[res][*i].push_back(states.size());
-	}
-	res = states.size();
-	states.push_back(NFAState(256, vi()));
-	break;
-      default:
-	states[res][(unsigned char)*i].push_back(states.size());
-	res = states.size();
-	states.push_back(NFAState(256, vi()));
-      }
-      if (*i)  ++i;
+      states = nextStates(states, *i++);
     }
-    return res;
+    return lastAccept;
   }
 
-public:
-  int accept;
-  
-  NFA(string& regexp) {
-    // Necessary due to the mean and evil way in which string works.
-    *(regexp.begin() + regexp.length()) = 0;
+  int newState() {
     states.push_back(NFAState(256, vi()));
-    string::const_iterator i = regexp.begin();
-    accept = parseRegExp(i, 0);
+    accepting.push_back(false);
+    return states.size() - 1;
   }
 
+  void newTransition(int from, int to, char c) { 
+    states[from][c].push_back(to);
+  }
   
-  /* Runs in O(n*m*log(n*m)) \subset O(N^2log(N)), where n is the size
-   * of S _after_ the epsilon closure is complete, m is the average
-   * number of epsilon-transitions from these states, and N is the
-   * total number of states in the NFA.
-   */
-  void epsilonClosure(StateSet& S) {
+  void epsClosure(StateSet& S) {
     queue<int> q;
     for (StateSet::iterator i = S.begin(); i != S.end(); ++i)
       q.push(*i);
     while (!q.empty()) {
-      vi epsStates = states[q.front()][0];
+      vi &epsStates = states[q.front()][0];
       q.pop();
       for (vi::iterator i = epsStates.begin(); i != epsStates.end(); ++i)
 	if (S.insert(*i).second)
@@ -95,15 +60,10 @@ public:
     }
   }
 
-
-  /* Runs in O(n*m*log(n*m)) \subset O(N^2log(N)), where n is the size
-   * of S, m is the average number of c-transitions in the states in
-   * S, and N is the total number of states in the NFA.
-   */
   StateSet nextStates(StateSet& S, char c) {
     StateSet res;
     for (StateSet::iterator i = S.begin(); i != S.end(); ++i) {
-      vi nStates = states[*i][(unsigned char)c];
+      vi &nStates = states[*i][(unsigned char)c];
       for (vi::iterator j = nStates.begin(); j != nStates.end(); ++j)
 	res.insert(*j);
     }
--- pattern/regexp.cpp
+++ pattern/regexp.cpp
@@ -1,85 +1,58 @@
 /* KTH ACM Contest Template Library
  *
- * Pattern Matching/Regular Expressions/RegExp -> NFA -> DFA
+ * Pattern Matching/Regular Expressions/RegExp -> NFA
  *
  * Credit:
  *   By Per Austrin, 2002-10-2?
  */
 
 
-#include "nfa.cc" // nfa.cc includes vector, and other necessities.
-#include <map>
+#include "nfa.cpp"
 
-/* Note: some general cleaning up between this file and nfa.cc is to
- *       be expected in the near future. I hope.
- */
+int parseRegExp(NFA& nfa, char* s, int entry);
 
-class RegExp {
-protected:
-  vector<vi> states;
-  vector<bool> accepting;
+NFA parseRegexp(const char* s) {
+  NFA res;
+  res.newState();
+  res.accepting[parseRegExp(res, s, 0)] = true;
+  return res;
+}
 
-public:
-  RegExp(string& regexp) {
-    map<StateSet, int> stateIdx;
-    vector<StateSet> stateSets;
-    unsigned int state = 0;
-    NFA nfa(regexp); // Create the NFA for the regexp
-    
-    /* Create initial state */
-    StateSet start;
-    start.insert(0);
-    nfa.epsilonClosure(start);
-    stateIdx[start] = 1;
-    stateSets.push_back(start);
-    states.push_back(vi(256, -1));
-    accepting.push_back(false);
 
-    /* Process states as they come */
-    while (state < states.size()) {
-      StateSet s = stateSets[state];
-      /* Is this an accepting state? */
-      if (s.count(nfa.accept))
-	accepting[state] = true;
-      /* Which chars trigger state transitions from this state? */
-      for (int i = 1; i < 256; ++i) {
-	StateSet nS = nfa.nextStates(s, i);
-	if (!nS.empty()) {
-	  nfa.epsilonClosure(nS);
-	  int& idx = stateIdx[nS];
-	  /* Is this a new state? */
-	  if (idx == 0) {
-	    states.push_back(vi(256, -1));
-	    accepting.push_back(false);
-	    stateSets.push_back(nS);
-	    idx = states.size();
-	  }
-	  /* Add state transition */
-	  states[state][i] = idx-1;
-	}
-      }
-      ++state;
+int parseRegExp(NFA& nfa, const char* s, int entry) {
+  int res = entry;
+  char literal = 0;
+  while (*i && *i != ')') {
+    int oldRes = res;
+    switch (*i | (*i == '"' ? 0 : literal)) {
+    case '"':
+      literal = ~literal;
+      break;
+    case '(':
+      res = parseRegExp(++i, res);
+      break;
+    case '|': {
+      int altEnd = parseRegExp(++i, entry);
+      res = nfa.newState();
+      nfa.newTransition(oldRes, res, 0);
+      nfa.newTransition(altEnd, res, 0);
+      --i;
+      break;
     }
-  }
-  
-
-  /* Returns the length of the longest string beginning in s which
-   * _completely matches_ the regular expression. If no prefix matches
-   * the regexp, the return value is string::npos.
-   *
-   * Time complexity: O(n), where n is the length of the string.
-   */
-  size_t match(string::const_iterator s) {
-    int state = 0;
-    string::const_iterator begin = s;
-    size_t lastAccept = string::npos;
-    while (state != -1) {
-      if (accepting[state])
-	lastAccept = distance(begin, s);
-      if (!*s)
-	break;
-      state = states[state][*s++];
+    case '*':
+      nfa.newTransition(res, entry, 0);
+      res = entry;
+      break;
+    case '[':
+      res = nfa.newState();
+      while (*++i != ']' && *i)
+	nfa.newTransition(oldRes, res, *i);
+      break;
+    default:
+      res = nfa.newState();
+      nfa.newTransition(oldRes, res, (unsigned char)*i);
     }
-    return lastAccept;
+    if (*i)  ++i;
   }
-};
+  return res;
+}
