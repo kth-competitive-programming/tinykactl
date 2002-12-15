599343df21a5356af4bc19bce2c0dbe9f2900921
mad33 <mad33@mad33.>
2002-12-15 16:54:06 +0100

    Added timetable for_edge funs.
    bk: 3dfca59e0BOEAVmrTQ9jXbSnQMgbvg

--- graph/for_edge.cpp
+++ graph/for_edge.cpp
@@ -1,5 +1,6 @@
 // example for_edge function objects:
 
+// one fun
 typedef vector<int> Vi; vector<Vi> g;
 struct onefun {
   template <class F> void operator()(int node, F &f) {
@@ -7,20 +8,47 @@ struct onefun {
     for (Vi::const_iterator i = l.begin(); i != l.end(); ++i)
       f(make_pair(*i, 1));
   }
-}
+};
 
+// weighted edge fun
 typedef vector<pair<int, int> > Vp; vector<Vp> h;
-struct lenfun {
+struct wefun {
   template <class F> void operator()(int node, F &f) {
     const Vp &l = h[node];
     for_each(l.begin(), l.end(), f);
   }
-}
+};
 
+// geometrical dist fun
 typedef vector<point<double> > VP; int n; VP pts(n);
 struct distfun {
   template <class F> void operator()(int node, F &f) {
     for (int i = 0; i < pts.size(); ++i)
-      f(i, dist(pts[node] - pts[i]));
+      f(make_pair(i, dist(pts[node] - pts[i])));
   }
-}
+};
+
+// the following funs fetches min[node] from inside f, which is a bit ugly:
+
+// for time-table searches without mod
+typedef vector<pair<int, pair<int, int> > > Vpp; vector<Vpp> g;
+struct stepfun {
+  template <class F> void operator()(int node, F &f) {
+    const Vi &l = g[node]; int t = f.min[node];
+    for (Vpp::const_iterator i = l.begin(); i != l.end(); ++i)
+      if (i->second.first >= t)
+	f(make_pair(i->first, i->second.first - t + i->second.second));
+  }
+};
+
+// for time-table searches
+typedef vector<pair<int, pair<int, int> > > Vpp; vector<Vpp> g;
+template <int MOD> struct modfun {
+  template <class F> void operator()(int node, F &f) {
+    const Vi &l = g[node]; int t = f.min[node];
+    for (Vpp::const_iterator i = l.begin(); i != l.end(); ++i)
+      f(make_pair(i->first,
+		  (i->second.first - t % MOD + MOD) % MOD +
+		  i->second.second));
+  }
+};
