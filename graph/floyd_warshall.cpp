a8643f911207397279de4c7323fe6c811a0be4cf
dary <dary@dary.zapto.org>
2003-02-16 20:46:51 +0100

    Fixed TeX errors. Floyd-Warshall now again uses -1 as no-path indicator.
    bk: 3e4feaab3owH-XEQYQB-odHkS0b5IA

--- graph/floyd_warshall.cpp
+++ graph/floyd_warshall.cpp
@@ -6,18 +6,18 @@
  *   By David Rydh
  */
 
-template<class V1, class V2, class T = int>
-void floyd_warshall( V1 &adj, V2 &path, int n, T inf = T(1<<29) ) {
+template<class V1, class V2>
+void floyd_warshall( V1 &adj, V2 &path, int n ) {
   for( int x=0; x<n; x++ )
     for( int y=0; y<n; y++ )
-      path[x][y] = y;
+      path[x][y] = -1;
 
   for( int m=0; m<n; m++ )
     for( int x=0; x<n; x++ )
-      if( adj[x][m] < inf )
+      if( adj[x][m] >= 0 )
 	for( int y=0; y<n; y++ )
-	  if( adj[m][y] < inf )
-	    if( adj[x][m] + adj[m][y] < adj[x][y] ) {
+	  if( adj[m][y] >= 0 )
+	    if( adj[x][y] < 0 || adj[x][m] + adj[m][y] < adj[x][y] ) {
 	      adj[x][y] = adj[x][m] + adj[m][y];
 	      path[x][y] = m;
 	    }
