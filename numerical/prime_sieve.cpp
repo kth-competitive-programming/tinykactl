d6b354ab0750a6a70f4463e8f5cb269bfc601c27
austrin <austrin@grey06.nada.kth.se>
2004-02-08 17:35:33 +0100

    All is landscape. New prime sieve.
    bk: 40266555Qol3GaPI3izzkxm_xjiFoQ

--- numerical/prime_sieve.cpp
+++ numerical/prime_sieve.cpp
@@ -1,35 +1,50 @@
 /* KTH ACM Contest Template Library
  *
- * Numerical/Number Theory/Soll Primes
+ * Numerical/Number Theory/Prime Sieve
  *
  * Credit:
- *   Eratosthenes
- *   By David Rydh
+ *   By Per
  */
 
-#include <vector>
+#include <algorithm>
+#include <cmath>
 
-struct prime_sieve {
-  char *v;
+using namespace std;
 
-  prime_sieve( int n ) {
-    int sn = (int)sqrt((double)n);
-    n /= 2;
-    v = (char*)malloc(n);
-    memset( v, 1, n );
+struct prime_sieve { 
+  static const int pregen = 3*5*7*11*13;
+  typedef unsigned char uchar;
+  typedef unsigned int uint;
+  uint n, sqrtn;
+  uchar *isprime;
+  int *prime, primes;
 
-    for( int i=0, p=3; i<sn; i++, p+=2 ) {
-      if( v[i] ) {
-	// p is a prime, remove all p-multiples
-	for( int j=i+p; j<n; j+=p )
-	  v[j] = 0;
-      }
-    }
-  }
+  prime_sieve(int _n): n(_n), sqrtn((int)ceil(sqrt((double)n))) {
+    int n0 = n >> 4;
+    prime = new int[max(5000,(int)(2.4*n/log(2.0*n)))];
+    prime[0] = 2; prime[1] = 3; prime[2] = 5;
+    prime[3] = 7; prime[4] = 11; prime[5] = 13;
+    primes = 6;
+    isprime = new uchar[n0];
+    memset(isprime, 255, n0); 
+    for (int j = 1, p = prime[j]; j < 6; p = prime[++j])
+      for (int i=(p*p-3)>>4, s=(p*p-3)/2 & 7; i<=pregen; i+=(s+=p)>>3, s&=7)
+	isprime[i] &= ~(1 << s); 
 
-  bool isprime( int m ) {
-    if( !(m&1) )
-      return (m==2);
-    return v[(m-3)/2];
-  }
+    for (int d = pregen, b = pregen+1; b < n0; b += d, d <<= 1)
+      memcpy(isprime + b, isprime + 1, (n0 < b + d) ? n0-b : d);
+    
+    for (uint p = 17, i = 0, s = 7; p < n; p += 2, i += ++s >> 3, s &= 7)
+      if (isprime[i] & (1 << s)) {
+	prime[primes++] = p;
+	if (p < sqrtn) {
+	  int ii = i, ss = s + (p-1)*p/2;
+	  for (uint pp = p*p; pp < n; pp += p<<1, ss += p) {
+	    ii += (ss >> 3);
+	    ss &= 7;
+	    isprime[ii] &= ~(1 << ss);
+	  }
+	} // end if
+      } // end if
+  } // end constructor
 };
