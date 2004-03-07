1805970e94a007d37a7742c00161e225636f1f03
austrin <austrin@grey06.nada.kth.se>
2004-03-07 18:21:56 +0100

    Geometry changes.
    bk: 404b5a34O5tpY1OI9U4Bk6tJ7XEyCA

--- geometry/hull/convex_hull.cpp
+++ geometry/hull/convex_hull.cpp
@@ -6,14 +6,15 @@
  *   Graham
  *   Based on [Intro to algorithms] and previous KACTL implementation.
  *   By Mattias de Zalenski
+ *   Bugfixed for World Finals 2004 by Per
  */
 
 template <class P>
 struct cross_dist_comparator {
   P o; cross_dist_comparator(P _o) : o(_o) { }
   bool operator ()(const P &p, const P &q) const {
-    typename P::coord_type c = cross(p-o, q-o);
-    return c != 0 ? c > 0 : dist2(p-o) > dist2(q-o);
+    typename P::coord_type c = (p-o).cross(q-o);
+    return c != 0 ? c > 0 : (p-o).dist2() > (q-o).dist2();
   }
 };
 
@@ -29,17 +30,20 @@ It convex_hull(It begin, It end) {
   // colinear points on the first line of the hull must be reversed
   It i = begin + 1;
   for (It j = i++; i != end; j = i++)
-    if (cross(*i-*begin, *j-*begin) != 0)
+    if ((*i-*begin).cross(*j-*begin) != 0)
       break;
   reverse(begin + 1, i);
   // place hull points first by doing a Graham scan
-  It r = begin + 2;
-  for (It i = begin + 3; i != end; ++i) {
-    // change < 0 to <= 0 if colinear points on the hull are not desired
-    while (cross(*r-*(r-1), *i-*(r-1)) < 0)
+  It r = begin + 1;
+  for (It i = begin + 2; i != end; ++i) {
+    // change < 0 to <= 0 if colinear points are not desired
+    while (r > begin && (*r - *(r-1)).cross(*i-*(r-1)) < 0)
       --r;
     swap(*++r, *i);
   }
+  // removing colinear points at the end of the hull
+  while (r-1 > begin && cross(*begin - *(r-1), *r - *(r-1)) == 0)
+    --r;
   // return the iterator past the last hull point
   return ++r;
 }
