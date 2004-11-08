1ad679026e97adfa6cbcfd12bcc1693b3030371c
austrin <austrin@plan4ray.nada.kth.se>
2004-11-08 10:52:33 +0100

    Added matching transformations and file i/o.
    bk: 418f41e1rC4hgEC-hm9BYbll1Zv6ow

--- contest/script.cpp
+++ contest/script.cpp
@@ -1,11 +1,9 @@
-echo 'g++ -Wall -o $1 $1.cc' > c
+echo 'g++ -ansi -Wall -o $1 $1.cc' > c
 echo 'cat > $1.in' > i
 echo 'cat > $1.ans' > o
-echo './$1 < $1.in | tee $1.out' > t
-echo './$1 | tee $1.out' > td
 echo 'diff $1.out $1.ans' > d
 echo 'a2ps --line-numbers=1 $1' > p
 echo 'cp Template.cc $1.cc' > n
 
-chmod +x c i o t td d p n
+chmod +x c i o d p n
 
