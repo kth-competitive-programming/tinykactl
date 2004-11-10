f7cb7ecad7d6b20b596085de5599516981c4f51c
austrin <austrin@plan4ray.nada.kth.se>
2004-11-10 08:55:28 +0100

    Some minor changes. Added LIS, rewrote some paragraphs.
    bk: 4191c970y3yuPyZcMNWo6uvOHU7prA

--- numtheory/pollard-rho.cpp
+++ numtheory/pollard-rho.cpp
@@ -7,34 +7,28 @@
  *   By Per Austrin
  */
 
-#include "gcd.h"
-#include "mulmod.h"
+#include "gcd and mulmod functions"
 
-template <class T>
-inline T pollard_step(T x, T N) { /* calc x^2+1 (mod N) */
-  T r = mulmod(x, x, N);
+template <class T> inline T pollard_step(T x, T N) { 
+  T r = mulmod(x, x, N); 
   if (++r == N) r = 0;
-  return r;
+  return r; /* r = x^2+1 (mod N) */
 }
 
 /* Returns a non-trivial factor of N. (Note that if N is
  * prime, pollard_rho never returns) */
-template <class T>
-inline T pollard_rho(T N, int maxiter = 500) {
-  T x = random() % N, y = x;
-  T d;
+template <class T> T pollard_rho(T N, int maxiter = 500) {
+  T x = random() % N, y = x, d;
   int iter = 0;
   if (!(N & 1)) return 2;  /* Check factor 2 */
   /* Small factor check. */
-  for (d = 3; d <= 9997; d += 2)
-    if (!(N % d))
-      return d;
+  for (d = 3; d <= 997; d += 2)
+    if (N % d == 0) return d;
 
-  d = 1;
-  while (d == 1) {
+  for (d = 1; d == 1; ) {
     /* Reseed if too many iterations passed. */
     if (iter++ == maxiter)
-      x = y = rand() % N, iter = 0;
+      x = y = random() % N, iter = 0;
     x = pollard_step(x, N);
     y = pollard_step(pollard_step(y, N), N);
     d = gcd(y - x, N);
