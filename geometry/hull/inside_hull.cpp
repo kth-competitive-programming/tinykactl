62769ed77d0db6de94b890941ae143f3b4e191df
austrin <austrin@b047914e-5d42-11dc-babe-f454b42e896f>
2006-04-03 06:51:23 +0000

    Fixed bug.

--- geometry/hull/inside_hull.cpp
+++ geometry/hull/inside_hull.cpp
@@ -40,8 +40,8 @@ int inside_hull_sub(It begin, It end, It i1, It i2, const P&t) {
       return 0;
     return 1;
   }
-  It i = (i1 + i2) / 2; //??
-  int side = sideof(p[0], p[i], t);
+  It i = (i1 + i2) / 2;
+  int side = sideof(*begin, *i, t);
   if (side > 0) return inside_hull_sub(begin, end, i, i2, t);
   else return inside_hull_sub(begin, end, i1, i, t);
 }
